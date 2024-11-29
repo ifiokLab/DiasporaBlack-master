228514099dc294edce90ffd4437cd0726e9c8ee2
ifiokLab <ifiokudoh77@gmail.com>
2024-11-29 08:29:19 +0100

    juyt

--- DiasporaBlack/settings.py
+++ DiasporaBlack/settings.py
@@ -213,7 +213,7 @@ AUTH_USER_MODEL = 'App.myuser'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/App/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 LOGIN_REDIRECT_URL = 'customer_login'
 
